b16db5313aa610c447d10f91467d2825b475e6ed
vee97 <33605479+vee97@users.noreply.github.com>
2018-01-22 18:55:45 -0500

    Updated communication code

--- Nano_Communication_Code_E.ino
+++ Nano_Communication_Code_E.ino
@@ -3,7 +3,7 @@
   DIGF 2005 - Atelier II
   Professor Nick Puckett
   Tuesday January 23, 2018
-
+  
   NANO COMMUNICATION CODE
   This code allows Nanos to read information from the serial port sent
   from the control panel, and display corresponding images/animations on
